f3f89d59aa04d6116f1b00961879e079bc9974af
dimitrisaronis1-dev <dimitris.aronis1@gmail.com>
2026-01-27 13:39:14 +0200

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -1026,7 +1026,7 @@ with right:
         pass
 
 st.markdown("---")
-st.markdown("### 👉 Ανέβασε το INPUT excel (μόνο 2 στήλες)")
+st.markdown("### 👉 Ανέβασε το INPUT excel (μόνο 3 στήλες)")
 
 uploaded = st.file_uploader(
     " ",
